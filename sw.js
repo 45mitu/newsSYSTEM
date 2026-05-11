fbf13f57c5d3c0a557b1bedbd28607b8517770f1
NewsBot <newsbot@local>
2026-05-12 00:04:21 +0900

    Update digest 2026-05-12

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='news-v20260512';
+const URLS=['./index.html','./manifest.json','./icon.svg'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(URLS)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(ca=>ca.put(e.request,c));return r}).catch(()=>caches.match(e.request)))});
